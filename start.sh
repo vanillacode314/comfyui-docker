d0659e88881c75274252b468e19b1e271a5cb0e4
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-06-27 15:06:22 +0530

    add ultimatesdupscale and comfyroll

--- start.sh
+++ start.sh
@@ -10,6 +10,6 @@ fi
 if [ ! -d "/app/ComfyUI/custom_nodes/ComfyUI-Manager/.git" ]; then
 	git clone https://github.com/ltdrdata/ComfyUI-Manager /app/ComfyUI/custom_nodes/ComfyUI-Manager
 	uv pip install --system -r /app/ComfyUI/custom_nodes/ComfyUI-Manager/requirements.txt
-	python3 /app/ComfyUI/custom_nodes/ComfyUI-Manager/cm-cli.py install ComfyUI-Custom-Scripts ComfyUI-Crystools SimpleWildcardsComfyUI ComfyUI-Inspire-Pack ComfyUI-Impact-Pack ComfyUI_IPAdapter_plus efficiency-nodes-comfyui rgthree-comfy comfyui-reactor-node
+	python3 /app/ComfyUI/custom_nodes/ComfyUI-Manager/cm-cli.py install ComfyUI-Custom-Scripts ComfyUI_Comfyroll_CustomNodes ComfyUI_UltimateSDUpscale ComfyUI-Crystools SimpleWildcardsComfyUI ComfyUI-Inspire-Pack ComfyUI-Impact-Pack ComfyUI_IPAdapter_plus efficiency-nodes-comfyui rgthree-comfy comfyui-reactor-node
 fi
 python3 /app/ComfyUI/main.py --listen --port "${PORT:-7860}" "$@"
