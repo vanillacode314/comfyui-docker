3ee64303b48881ac2bbc42d454ec5dc1dae4925c
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-08-04 18:21:31 +0530

    update flux setup

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+DOWNLOAD_FLUX=1
+
 if [ ! -d "/app/ComfyUI/.git" ]; then
 	git clone https://github.com/comfyanonymous/ComfyUI
 	if [ -n "$DOWNLOAD_FLUX" ]; then
@@ -24,8 +26,11 @@ if [ ! -d "/app/ComfyUI/.git" ]; then
 		-r https://raw.githubusercontent.com/ltdrdata/ComfyUI-Impact-Pack/Main/requirements.txt \
 		-r https://raw.githubusercontent.com/ltdrdata/ComfyUI-Impact-Subpack/main/requirements.txt \
 		-r https://raw.githubusercontent.com/ltdrdata/ComfyUI-Inspire-Pack/main/requirements.txt \
-		-r https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/requirements.txt
-	python3 /app/ComfyUI/custom_nodes/ComfyUI-Manager/cm-cli.py install ComfyUI-Custom-Scripts ComfyUI_Comfyroll_CustomNodes ComfyUI_UltimateSDUpscale ComfyUI-Crystools SimpleWildcardsComfyUI ComfyUI-Inspire-Pack ComfyUI-Impact-Pack efficiency-nodes-comfyui rgthree-comfy comfyui-reactor-node
+		-r https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/requirements.txt \
+		-r https://github.com/gokayfem/ComfyUI_VLM_nodes/raw/main/requirements.txt \
+		-r https://github.com/cubiq/ComfyUI_essentials/raw/main/requirements.txt \
+		-r https://github.com/Fannovel16/comfyui_controlnet_aux/raw/main/requirements.txt
+	python3 /app/ComfyUI/custom_nodes/ComfyUI-Manager/cm-cli.py install ComfyUI-Custom-Scripts ComfyUI_Comfyroll_CustomNodes ComfyUI_UltimateSDUpscale ComfyUI-Crystools SimpleWildcardsComfyUI ComfyUI-Inspire-Pack ComfyUI-Impact-Pack efficiency-nodes-comfyui rgthree-comfy comfyui-reactor-node ComfyUI_essentials ComfyUI_VLM_nodes comfyui_controlnet_aux
 fi
 
 python3 /app/ComfyUI/main.py --listen --port "${PORT:-7860}" "$@"
